b91f53b479c876114733f9b96525908ae82a0aa8
berco <bercob@gmail.com>
2016-09-14 15:25:24 +0200

    fix create thumbnail

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-    raise 'WelcomeController::index'
-    logger.info 'WelcomeController::index'
-    image = MiniMagick::Image.open('app/assets/images/other-product.jpg')
-    logger.info "MiniMagick::Image.open: #{image.inspect}"
-    logger.info "image.resize: #{image.resize '100x100'}"
-    logger.info "image.write: #{image.write 'app/assets/images/other-product-thumbnail.jpg'}"
-
+    Rails.logger.info 'WelcomeController::index'
+    image = MiniMagick::Image.open './app/assets/images/other-product.jpg'
+    image.resize '100x100'
+    image.write './app/assets/images/other-product-thumbnail.jpg'
   end
 end
