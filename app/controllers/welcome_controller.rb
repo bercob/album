004c5f3893153a1511d6ff7d329b532fc153f6ee
berco <bercob@gmail.com>
2016-09-14 16:19:53 +0200

    ignore public/thumbnails files

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    Rails.logger.info 'WelcomeController::index'
+    Rails.logger.debug 'WelcomeController::index'
     image = MiniMagick::Image.open 'app/assets/images/other-product.jpg'
     image.resize '100x100'
     image.write 'public/thumbnails/other-product-thumbnail.jpg'
