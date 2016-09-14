6f89fc0f92d06cfa98df112610b71773d6097d72
berco <bercob@gmail.com>
2016-09-14 15:48:37 +0200

    save thumbnails into public

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,8 @@
 class WelcomeController < ApplicationController
   def index
     Rails.logger.info 'WelcomeController::index'
-    image = MiniMagick::Image.open './app/assets/images/other-product.jpg'
+    image = MiniMagick::Image.open 'app/assets/images/other-product.jpg'
     image.resize '100x100'
-    image.write './app/assets/images/other-product-thumbnail.jpg'
+    image.write 'public/thumbnails/other-product-thumbnail.jpg'
   end
 end
