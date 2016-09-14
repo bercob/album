8392f0c87af404dcd042c313e51d535f61e1c738
berco <bercob@gmail.com>
2016-09-14 13:44:31 +0200

    change logs level to info

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,9 @@
 class WelcomeController < ApplicationController
   def index
     image = MiniMagick::Image.open('app/assets/images/other-product.jpg')
-    Rails.logger.debug "MiniMagick::Image.open: #{image.inspect}"
-    Rails.logger.debug "image.resize: #{image.resize '100x100'}"
-    Rails.logger.debug "image.write: #{image.write 'app/assets/images/other-product-thumbnail.jpg'}"
+    logger.info "MiniMagick::Image.open: #{image.inspect}"
+    logger.info "image.resize: #{image.resize '100x100'}"
+    logger.info "image.write: #{image.write 'app/assets/images/other-product-thumbnail.jpg'}"
 
   end
 end
