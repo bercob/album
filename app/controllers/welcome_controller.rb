e759687cd5ff474fc81b79a63d374441e04faaea
berco <bercob@gmail.com>
2016-09-14 13:17:04 +0200

    debug creating thumbnail

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,9 @@
 class WelcomeController < ApplicationController
   def index
     image = MiniMagick::Image.open('app/assets/images/other-product.jpg')
-    image.resize '100x100'
-    image.write 'app/assets/images/other-product-thumbnail.jpg'
+    Rails.logger.debug "MiniMagick::Image.open: #{image.inspect}"
+    Rails.logger.debug "image.resize: #{image.resize '100x100'}"
+    Rails.logger.debug "image.write: #{image.write 'app/assets/images/other-product-thumbnail.jpg'}"
 
   end
 end
