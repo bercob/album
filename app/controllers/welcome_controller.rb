0d1a4018623ff70f5ae81107aadddb8dde41e39f
berco <bercob@gmail.com>
2016-09-14 10:54:07 +0200

    change magick to mini_magick gem

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-    image = Magick::Image.read('app/assets/images/other-product.jpg').first
+    image = MiniMagick::Image.open('app/assets/images/other-product.jpg')
+    image.resize '100x100'
+    image.write 'app/assets/images/other-product-thumbnail.jpg'
+
   end
 end
